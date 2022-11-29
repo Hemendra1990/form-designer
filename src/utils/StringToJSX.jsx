2815e38b043c8a26e21fddf43eaf6bedd6d85b75
hemendrasethi <hemendra.sethi@welltok.com>
2022-11-30 01:14:26 +0530

    Calling event from cell template click done

--- src/utils/StringToJSX.jsx
+++ src/utils/StringToJSX.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {Button} from "primereact/button";
+
+
+const getReactComps = {
+  "button": Button,
+}
+
+const executeEvent = (eventName, row) => {
+  console.log('execute event on grid item click...');
+}
 
 let getNodes = (str) =>
   new DOMParser().parseFromString(str, "text/html").body.childNodes;
@@ -18,13 +28,36 @@ let createJSX = (nodeArray, rowData) => {
           });
           attributeObj[attribute.name] = styleObj;
         } else {
-          attributeObj[attribute.name] = attribute.nodeValue;
+          if(attribute.name === 'classname') {
+            attributeObj["className"] = attribute.nodeValue;
+          } else if(attribute.name === 'onclick') {
+            /* attributeObj["onClick"] = new Function('event',`
+              console.log("hello");
+              alert(JSON.stringify('1'));
+            `); */
+            /* attributeObj["onClick"] = new Function('event',`
+            debugger;
+              console.log("hello");
+              console.log(rowData);
+            `); */
+            attributeObj["onClick"] = (e) => {
+              debugger;
+              const fun = new Function('event', 'row', 'nodeVal', `
+                              debugger;
+                              console.log(nodeVal);
+                              let t = new Function('row', nodeVal);
+                              return t()`);
+              fun(e, rowData, attribute.nodeValue);
+            };
+          } else {
+            attributeObj[attribute.name] = attribute.nodeValue;
+          }
         }
       });
     }
     return localName
       ? React.createElement(
-          localName,
+          getReactComps[localName],
           attributeObj,
           childNodes && Array.isArray(Array.from(childNodes))
             ? createJSX(Array.from(childNodes), rowData)
