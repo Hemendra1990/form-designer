9cf5991bf063931e68bd922bc7587ca802497037
Hemendra <hemendra.sethi@bipros.com>
2022-12-05 19:14:19 +0530

    Added deep clone utility method

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -20,3 +20,34 @@ export const evaluateCellTemplate = new Function('row', 'template', `
   const evaluate = new Function('row', template);
   return evaluate(row);
 `); /*eslint no-new-func: */
+
+/**
+ * Deep cloning a object supports circular dependency
+ * @param {*} obj 
+ * @returns 
+ */
+const deepClone = (obj) => {
+  if(structuredClone) {
+    return structuredClone(obj);
+  } else {
+    return deepCloneNative(obj);
+  }
+}
+
+/* https://stackoverflow.com/questions/4459928/how-to-deep-clone-in-javascript */
+const deepCloneNative = (obj, hash = new WeakMap()) {
+  if (Object(obj) !== obj) return obj; // primitives
+  if (hash.has(obj)) return hash.get(obj); // cyclic reference
+  const result = obj instanceof Set ? new Set(obj) // See note about this!
+               : obj instanceof Map ? new Map(Array.from(obj, ([key, val]) => 
+                                      [key, deepClone(val, hash)])) 
+               : obj instanceof Date ? new Date(obj)
+               : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
+               // ... add here any specific treatment for other classes ...
+               // and finally a catch-all:
+               : obj.constructor ? new obj.constructor() 
+               : Object.create(null);
+  hash.set(obj, result);
+  return Object.assign(result, ...Object.keys(obj).map(
+      key => ({ [key]: deepClone(obj[key], hash) }) ));
+}
