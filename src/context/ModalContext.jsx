7d9b1f1b3eec694743baae9e0a6a66ed6b2190d8
Hemendra <hemendra.sethi@bipros.com>
2022-12-14 23:53:20 +0530

    able to load report inside popup. But events are not working for the child report loaded in the popup

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -1,6 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useCallback, useContext, useState } from "react";
 import { createContext } from "react";
 import { Dialog } from "primereact/dialog";
+import { TEST_REPORTS_FOR_TESTING } from "../tests/report";
+import getComponent from "../constants/HemendraConstants";
+import { useMetaContext } from "./MetaContext";
 
 export const ModalContext = createContext();
 
@@ -15,6 +18,68 @@ const uniqueModalId = (() => {
 
 export const ModalContextProvider = ({ children }) => {
   const [modals, setModals] = useState([]);
+  const [containerMeta, setContainerMeta] = useState({});
+
+  const meta = useMetaContext();
+  const [elements, setElements] = useState([]);
+
+  function initializeComponent(reportElements) {
+    if (!reportElements) {
+      return;
+    }
+    reportElements.forEach((el) => {
+      el.component = getComponent(el.type);
+      if (
+        el.attributes &&
+        el.attributes.children &&
+        el.attributes.children.length > 0
+      ) {
+        initializeComponent(el.attributes.children);
+      }
+    });
+  }
+
+  const createChildElement = useCallback((child, index) => {
+    let ref = child.ref;
+    if (!ref) {
+      ref = React.createRef();
+    }
+    let cMeta;
+    if (Object.keys(containerMeta).length > 0) {
+      cMeta = containerMeta;
+    } else {
+      cMeta = meta;
+    }
+    const reactComponent = React.createElement(child.component, {
+      ref: ref,
+      key: index + 1,
+      name: `${child.name}`,
+      meta: cMeta,
+      element: child,
+      enteredValue: "",
+    });
+    child.ref = ref;
+    return reactComponent;
+  });
+
+  const loadReport = (content) => {
+    const resourceId = content.resourceId || "rid-0123";
+    const json = TEST_REPORTS_FOR_TESTING.find(
+      (trpt) => trpt.id === resourceId
+    ).json; //we will be getting the json string from the server
+    const report = JSON.parse(json);
+    initializeComponent(report.elements);
+    setContainerMeta(report);
+    setElements(report.elements);
+    return (
+      <Fragment>
+        HELLO LOAD REPORT
+        {elements.map((element, index) => {
+          return createChildElement(element, index);
+        })}
+      </Fragment>
+    );
+  };
 
   const actions = {
     push(content, onHideCallback) {
@@ -34,6 +99,7 @@ export const ModalContextProvider = ({ children }) => {
             {" "}
             {/*TODO  What if I want to handle the event on Modal close */}
             <p>{content.popupText}</p>
+            {loadReport(content)}
           </Dialog>
         );
       } else {
