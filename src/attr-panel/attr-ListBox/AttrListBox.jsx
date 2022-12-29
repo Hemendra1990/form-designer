71827b16e8cf25741cde016f90c32b4615dca26d
Hemendra <hemendra.sethi@bipros.com>
2022-12-29 19:10:50 +0530

    Code Review and Bug Fixes

--- src/attr-panel/attr-ListBox/AttrListBox.jsx
+++ src/attr-panel/attr-ListBox/AttrListBox.jsx
@@ -1,124 +1,162 @@
-import React, { useState } from 'react';
-import { Dropdown } from 'primereact/dropdown';
+import React, { useEffect, useState } from "react";
+import { Dropdown } from "primereact/dropdown";
 import { InputText } from "primereact/inputtext";
-import { Checkbox } from 'primereact/checkbox';
-import { element } from 'prop-types';
-
+import { Checkbox } from "primereact/checkbox";
 
 const AttrListBox = (props) => {
-    const { meta, eventOptions, updateClass, handleAttributeChange } = props;
-    const currAttribute = meta?.currentElement?.attributes;
+  const { meta, eventOptions, updateClass, handleAttributeChange } = props;
+  const currAttribute = meta?.currentElement?.attributes;
 
-    const [checked, setChecked] = useState(false);
-    const [multiple, setMultiple] = useState(false);
-    const [filter, setFilter] = useState(false);
-    const [value, setValue] = useState({});
-    const [onChangeEvent, setOnChangeEvent] = useState("");
-    const [onFilterChange, setOnFilterChange] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [multiple, setMultiple] = useState(false);
+  const [filter, setFilter] = useState(false);
+  const [tooltip, setTooltip] = useState("");
+  const [filterby, setFilterBy] = useState("");
+  const [onChangeEvent, setOnChangeEvent] = useState("");
+  const [onFilterChange, setOnFilterChange] = useState("");
 
-    const handleOnChangeEvent = (e) => {
-        setOnChangeEvent(e.value);
-        currAttribute.onchangeevent = e.value;
-    };
-    const handleFilterValueChangeEvent = (e) => {
-        setOnFilterChange(e.value);
-        currAttribute.onfiltervaluechange = e.value;
-    };
+  useEffect(() => {
+    setDisabled(currAttribute.disabled || false);
+    setMultiple(currAttribute.multiple || false);
+    setFilter(currAttribute.filter || false);
+    setTooltip(currAttribute.tooltip || false);
+    setOnChangeEvent(currAttribute.onChangeEvent || false);
+    setOnFilterChange(currAttribute.onFilterChange || false);
+  }, []);
 
-    return (
-        <>
-            <div className="field col-12">
-                <label htmlFor="controlId" className="block">Control ID</label>
-                <InputText name="placeholder" style={{ width: '100%' }} value={meta.currentElement.id} disabled />
-            </div>
-            <div className="field-checkbox">
-                <Checkbox
-                    inputId="binary"
-                    checked={checked}
-                    onChange={(e) => {
-                        setChecked(e.checked);
-                        meta.currentElement.attributes.disabled = e.checked;
-                    }}
-                />
-                <label htmlFor="disabled" className="p-checkbox-label">Disabled</label>
-            </div>
-            <div className="field-checkbox">
-                <Checkbox
-                    inputId="binary"
-                    checked={multiple}
-                    onChange={(e) => {
-                        setMultiple(e.checked);
-                        meta.currentElement.attributes.multiple = e.checked;
-                    }}
-                />
-                <label htmlFor="multiple" className="p-checkbox-label">Multiple</label>
-            </div>
-            <div className="field-checkbox">
-                <Checkbox
-                    inputId="binary"
-                    checked={filter}
-                    onChange={(e) => {
-                        setFilter(e.checked);
-                        meta.currentElement.attributes.filter = e.checked;
-                    }}
-                />
-                <label htmlFor="filter" className="p-checkbox-label">Filter</label>
-            </div>
-            <div className="field col-12">
-                <label htmlFor="filterby">FilterBy</label>
-                <InputText
-                    id="in"
-                    value={meta.currentElement.attributes.filterBy}
-                    style={{ width: '100%' }}
-                    name="filterby"
-                    onChange={handleAttributeChange}
-                />
-            </div>
-            <div className="field col-12">
-                <label htmlFor="tooltip">Tooltip</label>
-                <InputText
-                    style={{ width: '100%' }}
-                    tooltipOptions={{ position: 'right', mouseTrack: true }}
-                    onChange={(e) => {
-                        setValue(e.target.value);
-                        meta.currentElement.attributes.tooltip = e.target.value
-                    }}
-                />
-            </div>
-            <div className="field col-12">
-                <label htmlFor="onChangeEvent" className="block">onChange</label>
-                <Dropdown
-                    style={{ width: '100%' }}
-                    name="onchangeevent"
-                    value={onChangeEvent}
-                    options={eventOptions}
-                    placeholder="Select a onChange Event"
-                    onChange={e => { handleOnChangeEvent(e) }}
-                />
-            </div>
-            <div className="field col-12">
-                <label htmlFor="onFilterValueChange" className="block">onFilterValueChange</label>
-                <Dropdown
-                    style={{ width: '100%' }}
-                    name="onfiltervaluechange"
-                    value={onFilterChange}
-                    options={eventOptions}
-                    placeholder="Select a onFilterChange Event"
-                    onChange={e => { handleFilterValueChangeEvent(e) }}
-                />
-            </div>
-            <div className="field col-12">
-                <label className="block">Class</label>
-                <InputText
-                    style={{ width: '100%' }}
-                    name="className"
-                    placeholder="col-12 md:col-6 lg:col-3"
-                    value={currAttribute?.className || ""}
-                    onChange={updateClass}
-                />
-            </div>
-        </>
-    )
+  const handleOnChangeEvent = (e) => {
+    setOnChangeEvent(e.value);
+    currAttribute.onchangeevent = e.value;
+    handleAttributeChange(e);
+  };
+  const handleFilterValueChangeEvent = (e) => {
+    setOnFilterChange(e.value);
+    currAttribute.onfiltervaluechange = e.value;
+    handleAttributeChange(e);
+  };
+
+  return (
+    <>
+      <div className="field col-12">
+        <label htmlFor="controlId" className="block">
+          Control ID
+        </label>
+        <InputText
+          name="controlId"
+          style={{ width: "100%" }}
+          value={meta.currentElement.id}
+          disabled
+        />
+      </div>
+      <div className="field-checkbox">
+        <Checkbox
+          name="disabled"
+          inputId="binary"
+          checked={disabled}
+          onChange={(e) => {
+            setDisabled(e.checked);
+            handleAttributeChange(e);
+          }}
+        />
+        <label htmlFor="disabled" className="p-checkbox-label">
+          Disabled
+        </label>
+      </div>
+      <div className="field-checkbox">
+        <Checkbox
+          name="multiple"
+          inputId="binary"
+          checked={multiple}
+          onChange={(e) => {
+            setMultiple(e.checked);
+            handleAttributeChange(e);
+          }}
+        />
+        <label htmlFor="multiple" className="p-checkbox-label">
+          Multiple
+        </label>
+      </div>
+      <div className="field-checkbox">
+        <Checkbox
+          name="filter"
+          inputId="binary"
+          checked={filter}
+          onChange={(e) => {
+            setFilter(e.checked);
+            handleAttributeChange(e);
+          }}
+        />
+        <label htmlFor="filter" className="p-checkbox-label">
+          Filter
+        </label>
+      </div>
+      <div className="field col-12">
+        <label htmlFor="filterby">Filter By</label>
+        <InputText
+          value={filterby}
+          style={{ width: "100%" }}
+          name="filterby"
+          onChange={(e) => {
+            setFilterBy(e.target.value);
+            handleAttributeChange(e);
+          }}
+        />
+      </div>
+      <div className="field col-12">
+        <label htmlFor="tooltip">Tooltip</label>
+        <InputText
+          name="tooltip"
+          style={{ width: "100%" }}
+          value={tooltip}
+          tooltipOptions={{ position: "right", mouseTrack: true }}
+          onChange={(e) => {
+            setTooltip(e.target.value);
+            handleAttributeChange(e);
+          }}
+        />
+      </div>
+      <div className="field col-12">
+        <label htmlFor="onChangeEvent" className="block">
+          onChange
+        </label>
+        <Dropdown
+          style={{ width: "100%" }}
+          name="onchangeevent"
+          value={onChangeEvent}
+          options={eventOptions}
+          placeholder="Select a onChange Event"
+          onChange={(e) => {
+            handleOnChangeEvent(e);
+          }}
+        />
+      </div>
+      <div className="field col-12">
+        <label htmlFor="onFilterValueChange" className="block">
+          onFilterValueChange
+        </label>
+        <Dropdown
+          style={{ width: "100%" }}
+          name="onfiltervaluechange"
+          value={onFilterChange}
+          options={eventOptions}
+          placeholder="Select a onFilterChange Event"
+          onChange={(e) => {
+            handleFilterValueChangeEvent(e);
+          }}
+        />
+      </div>
+      <div className="field col-12">
+        <label className="block">Class</label>
+        <InputText
+          style={{ width: "100%" }}
+          name="className"
+          placeholder="col-12 md:col-6 lg:col-3"
+          value={currAttribute?.className || ""}
+          onChange={updateClass}
+        />
+      </div>
+    </>
+  );
 };
 
 export default AttrListBox;
--- src/components/HDListBox.jsx
+++ src/components/HDListBox.jsx
@@ -1,92 +1,69 @@
-import React, { useState } from 'react';
-import { ListBox } from 'primereact/listbox';
-import EventExecutor from '../service/EventExecutor';
+import React, { useEffect, useState } from "react";
+import { ListBox } from "primereact/listbox";
+import EventExecutor from "../service/EventExecutor";
 
 const HDListBox = React.forwardRef((props, ref) => {
-    const { element } = props;
-    const [selectedCity, setSelectedCity] = useState(null);
+  const { element } = props;
+  const [selectedValue, setSelectedValue] = useState(null);
 
-    const handleOnChangeEvent = (e) => {
-        if(element.attributes && element.attributes.onchangeevent) {
-            EventExecutor.executeEvent(props.meta, element.attributes.onchangeevent, null, null);
-        }
+  console.log(element);
+
+  const handleOnChangeEvent = (e) => {
+    if (element.attributes && element.attributes.onchangeevent) {
+      EventExecutor.executeEvent(
+        props.meta,
+        element.attributes.onchangeevent,
+        null,
+        null
+      );
     }
-      const handleFilterValueChangeEvent = (e) => {
-        if(element.attributes && element.attributes.onfiltervaluechange) {
-            EventExecutor.executeEvent(props.meta, element.attributes.onfiltervaluechange, null, null);
-        }
+  };
+  const handleFilterValueChangeEvent = (e) => {
+    if (element.attributes && element.attributes.onfiltervaluechange) {
+      EventExecutor.executeEvent(
+        props.meta,
+        element.attributes.onfiltervaluechange,
+        null,
+        null
+      );
     }
-    
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
-    ];
+  };
 
-    const countries = [
-        { name: 'Australia', code: 'AU' },
-        { name: 'Brazil', code: 'BR' },
-        { name: 'China', code: 'CN' },
-        { name: 'Egypt', code: 'EG' },
-        { name: 'France', code: 'FR' },
-        { name: 'Germany', code: 'DE' },
-        { name: 'India', code: 'IN' },
-        { name: 'Japan', code: 'JP' },
-        { name: 'Spain', code: 'ES' },
-        { name: 'United States', code: 'US' }
-    ];
+  const listOptions = [
+    /* This value will come from user's input or from datasource in future */
+    { label: "New York", value: "NY", country: "USA" },
+    { label: "Rome", value: "RM", country: "Italy" },
+    { label: "London", value: "LDN", country: "UK" },
+    { label: "Istanbul", value: "IST", country: "Turkey" },
+    { label: "Paris", value: "PRS", country: "France" },
+  ];
 
-    const groupedCities = [
-        {
-            label: 'Germany', code: 'DE',
-            items: [
-                { label: 'Berlin', value: 'Berlin' },
-                { label: 'Frankfurt', value: 'Frankfurt' },
-                { label: 'Hamburg', value: 'Hamburg' },
-                { label: 'Munich', value: 'Munich' }
-            ]
-        },
-        {
-            label: 'USA', code: 'US',
-            items: [
-                { label: 'Chicago', value: 'Chicago' },
-                { label: 'Los Angeles', value: 'Los Angeles' },
-                { label: 'New York', value: 'New York' },
-                { label: 'San Francisco', value: 'San Francisco' }
-            ]
-        },
-        {
-            label: 'Japan', code: 'JP',
-            items: [
-                { label: 'Kyoto', value: 'Kyoto' },
-                { label: 'Osaka', value: 'Osaka' },
-                { label: 'Tokyo', value: 'Tokyo' },
-                { label: 'Yokohama', value: 'Yokohama' }
-            ]
-        }
-    ];
+  useEffect(() => {
+    element.attributes = element.attributes || {};
+    element.attributes.multiple = false;
+  }, []);
 
-    const items = Array.from({ length: 100000 }).map((_, i) => ({ label: `Item #${i}`, value: i }));
-
-    return (
-        <ListBox value={selectedCity} 
-        options={cities}        
+  return (
+    <>
+      <h1>Mulitple : {element.attributes?.multiple}</h1>
+      <ListBox
+        value={selectedValue}
+        options={listOptions}
         onChange={(e) => {
-            setSelectedCity(e.value);
-            handleOnChangeEvent(e)
-            handleFilterValueChangeEvent(e)
-        }} 
-        optionLabel="name"               
-        disabled={element.attributes.disabled}
-        multiple={element.attributes.multiple}
-        filter={element.attributes.filter}
-        filterBy={element.attributes.filterBy}
-        tooltip={element.attributes.tooltip}
-        />
-    );
-})
+          setSelectedValue(e.value);
+          handleOnChangeEvent(e);
+          handleFilterValueChangeEvent(e);
+        }}
+        optionValue={element.attributes.optionValue || "value"}
+        optionLabel={element.attributes.optionLabel || "label"}
+        disabled={element.attributes?.disabled || false}
+        filter={element.attributes?.filter || false}
+        filterBy={element.attributes?.filterby}
+        tooltip={element.attributes?.tooltip}
+      />
+      {/* multiple={element.attributes?.multiple || false} */}
+    </>
+  );
+});
 
 export default HDListBox;
- 
