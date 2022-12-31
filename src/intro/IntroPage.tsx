54643fccaa530b0eedfb8f052b1e915b01fddbe6
Hemendra <hemendra.sethi@bipros.com>
2022-12-31 22:23:11 +0530

    Developers added.

--- src/intro/IntroPage.tsx
+++ src/intro/IntroPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './IntroPage.css';
+import {Button} from "primereact/button";
 
 function IntroPage() {
     const navigate = useNavigate();
